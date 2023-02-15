a2fc9447fa4bea3a38e8b66a9b5948d0672ff889
Sara Leya <kolesnichenkova_@mail.ru>
2023-02-15 17:32:27 +0300

    modified:   hw-2-7.html renamed:    wh-2-7.js -> hw-2-7.js

---
+++ hw-2-7.js
@@ -0,0 +1,26 @@
+/*1 */
+const line = 'AfjhdEnfjbgWWWWWw';
+console.log(line.toUpperCase());
+
+/*2 */
+
+const life = ['мыло', 'веревка', 'револьвер', 'снотворное', 'нервы', 'Ненависть', 'депреССия', 'агония', 'апатия', 'рефлексия'];
+
+const search = 'не';
+const result = [];
+function sos (life, search){
+    for(let item of life){
+        if(item.toLowerCase().startsWith(search.toLowerCase())){
+            result.push(item);
+        }
+    }  
+}
+
+sos(life, search);
+
+console.log(result);
+
+
+/*3*/
+let numer = '32.58884';
+
