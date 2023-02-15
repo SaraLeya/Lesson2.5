2600c4044409bbfcb0a18cf19d3beb97fb493a2d
SaraLeya <kolesnichenkova_@mail.ru>
2023-02-15 14:44:39 +0300

    Урок 2.7

---
+++ wh-2-7.js
@@ -0,0 +1,25 @@
+/*1 */
+const line = 'AfjhdEnfjbgWWWWWw';
+console.log(line.toUpperCase());
+
+/*2 */
+/*
+const life = ['мыло', 'веревка', 'револьвер', 'снотворное', 'нервы', 'Ненависть', 'депреССия', 'агония', 'апатия', 'рефлексия',
+['психотерапия', 'увольнение', 'переезд']
+];
+const search = 'не';
+const result = [];
+
+life.forEach (answer) => {
+    if(life.toLowerCare().StartWith(search.toLowerCare())){
+        result[result.length] = answer;
+    }
+}
+
+console.log(result);
+
+*/
+
+/*3*/
+let numer = '32.58884';
+
