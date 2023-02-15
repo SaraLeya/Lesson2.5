ee891c7fb870488d37cf600eeff43742831335d4
Sara Leya <kolesnichenkova_@mail.ru>
2023-02-15 19:41:49 +0300

    new file:   chern.js 	deleted:    hw-2-6.js modified:   hw-2-7.js

--- hw-2-7.js
+++ hw-2-7.js
@@ -20,7 +20,71 @@ sos(life, search);
 
 console.log(result);
 
-
 /*3*/
-let numer = '32.58884';
+let num = 32.58884;
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
 
+/*4*/
+let arr = [52, 53, 49, 77, 21, 32];
+console.log(Math.max.apply(null, arr));
+console.log(Math.min.apply(null, arr));
+
+/*5*/
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+function trh () {
+console.log(getRandomInt(0, 10))
+}
+
+trh();
+
+
+/*8*/
+let curDate = new Date();
+console.log(curDate);
+
+/*9*/
+let currectDate = new Date();
+console.log(+currectDate);
+let day73 = 73 * 24 * 60 * 60 * 1000;
+
+let searchDate = currectDate + day73;
+
+let dayAfter73 = new Date(searchDate);
+
+console.log(dayAfter73);
+
+/*6*/
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  st = Math.round(prompt('num'));
+  const mas = [];
+  function flag (st) {
+for(i = 0; i < Math.round(st / 2); i++){
+    mas.push(getRandomInt(0, st));
+}
+}
+
+flag(st);
+
+console.log(mas);
+
+/*7 не вышло*/
+const mas2 = [];
+let d = prompt('num1');
+    let u = prompt('num2');
+function lll (d, u){
+    Math.round(d);
+    Math.round(u);
+    mas2.push(Math.round(Math.random (d, u)));
+}
+
+lll();
+
+console.log(mas2);
