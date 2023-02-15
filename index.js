a3bcfc0e04da18b412f0b77834b29d90e64881a4
Sara Leya <kolesnichenkova_@mail.ru>
2023-02-15 20:11:11 +0300

    new file:   _words.scss 	deleted:    in.js 	modified:   index.js 	modified:   style.scss 	new file:   words.html

--- index.js
+++ index.js
@@ -1,84 +1,4 @@
-/*Задание 6*/
 
-let shit = () =>{
-    let n = Number(prompt('Введите число'));
-    if (n === || isNaN(n) || n === undefined) {
-        let n = n**3;
-        alert(`n в кубе равняется ${f}`);
-    } else {
-        alert('Переданный параметр не является числом');
-    }
- }
-
- shit();
-
-/*Задание 5*/
-
-let red = (num3, num4) =>{
-    num3 = Number(num3);
-    num4 = Number(num4);
-if (num3 === isNaN(num3) || num3 === undefined|| num4 === || num4 === isNaN(num4) || num4 === undefined){
-    console.log('Одно или оба значения не являются числом');
-} else {
-let result = num3*num4;
-console.log(result);
-}
-}
-
-red(2,3);
-
-/*Задание 4*/
-let age = () => {
-    let userAnswer = prompt ('Сколько Вам лет?');
-if (userAnswer < 0 || isNaN(userAnswer)) {
-    alert('Вы ввели неправильное значение');
-} else if (userAnswer <= 12) {
-    alert('Привет, друг!');
-} else {
-    alert('Добро пожаловать!');
-}
-}
-
-age();
-
-/*Задание 3*/
-/*Задание 3.1. */
-let squaring = (num1) => сonsole.log(num1**2);
-
-squaring(2);
-
-/*3.2. */
-let squaring1 = (number1) => {
-    return number1**2;
-} 
-
-console.log(squaring1(4));
-
-/*Задание 2*/
-
-let evenNumber =(n) => {
-    if ( n % 2 == 0){
-        return `Число четное`;
-    } else{
-        return `Число нечетное`;
-    }
-}
-
-console.log(evenNumber(4));
-
-/*Задание 1*/
-
-let bigest =(a,b) => {
-    if ( a > b){
-        return b;
-    } else{
-        return a;
-    }
-}
-
-bigest(7, 2);
-
-/*Задание 7*/
 function game1 (){
     let monthNumber = prompt('Введите число месяца');
     switch (monthNumber) {
@@ -105,3 +25,6 @@ function game1 (){
         default: alert('Нет такого');
     }
     }
+const fruts = [];
+
+    function game2 ()
