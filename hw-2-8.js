5b510ab679b8560bfdde29787e5f44d4c97487a9
Sara Leya <kolesnichenkova_@mail.ru>
2023-02-25 19:57:04 +0300

    modified:   hw-2-8.html 	modified:   hw-2-8.js

--- hw-2-8.js
+++ hw-2-8.js
@@ -1,4 +1,21 @@
-/*1*/
+/*1 не работает*/
+/* const arr = [2, 5, 10, 200, 50, 30, 5];
+
+const result (arra, callback) => {
+    Math.round(callback());
+    console.log(Math.round(callback));
+}
+
+let sum () =>{
+    massiv.arr(i=>x+=i, x=0).reverse()[0];
+}
+
+let mult () => {
+    arr.reduce((a,b)=>a*b);
+}
+
+getResult(arr, sum);
+getResult(arr, mult);
 
 /*2*/
 const users = [
@@ -9,10 +26,67 @@ const users = [
 	{name: 'Bob', age: 44}
 ];
 
+users.sort((a,b) => a.age - b.age);
 
-/*3*/
+console.log(users);
 
-/*4*/
+/*3 да что не так???*/
+/*const arr = [3, 4, 5, 5, 6, 'two', 74];
+
+let each(arra, callback) => {
+callback(arra);
+}
+
+const reversArr(arr) => {
+    arr.reverse();
+}
+
+const toNumberArr (arr) => {
+    for(i=0; i < length.arr; i++){
+        if(isNaN(+i)){
+            delete arr[i];
+        }
+    }
+}
+
+console.log(each(arr, reversArr));
+console.log(each(arr, toNumberArr));
+
+/*4 а вот тут получилось*/
+const timer = (deadline) => {
+
+    let time = deadline;
+    const interval = setInterval(() => {
+        time -= 3;
+        console.log(new Date());
+    }, 3000);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log('30 секунд прошло')
+    }, deadline * 1000)
+};
+
+
+timer(30000);
 
 /*5*/
+function calling() {
+    console.log('Звоню!')
+};
 
+function beeps() {
+    setTimeout(() => {
+        console.log('Идут гудки...')
+    }, 1000);
+}
+
+function talk() {
+    setTimeout(() => {
+    console.log('Разговор')
+}, 2000);
+}
+
+calling();
+beeps();
+talk();
