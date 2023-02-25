00e1784c873212b75df81ce183fafd6082fb530a
Sara Leya <kolesnichenkova_@mail.ru>
2023-02-25 16:13:01 +0300

    new file:   hw-2-8.html new file:   hw-2-8.js

---
+++ hw-2-8.js
@@ -0,0 +1,18 @@
+/*1*/
+
+/*2*/
+const users = [
+	{name: 'Jon', age: 22},
+	{name: 'Richard', age: 18},
+	{name: 'Anton', age: 32},
+	{name: 'Lida', age: 23},
+	{name: 'Bob', age: 44}
+];
+
+
+/*3*/
+
+/*4*/
+
+/*5*/
+
